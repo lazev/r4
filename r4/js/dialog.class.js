dff888d5dd62e786e0389c1420a61210da42f20c
Vini Lazev <vinicius@zipline.com.br>
2021-06-25 14:38:21 -0300

    Removida cor no * do CSS; Troca de seta de fechar o Dialog

--- r4/js/dialog.class.js
+++ r4/js/dialog.class.js
@@ -76,7 +76,7 @@ const Dialog = {
 			if(ephemeral) over.setAttribute('ephemeral', 'true');
 
 			let closer = document.createElement('div');
-			closer.innerHTML = '<svg id="i-caret-left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="16" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"><path d="M22 30 L6 16 22 2 Z" /></svg>';
+			closer.innerHTML = '<b>&#8678;</b>';
 			closer.style.float = 'left';
 			closer.setAttribute('target', idElem);
 			closer.classList.add('R4DialogCloser');
